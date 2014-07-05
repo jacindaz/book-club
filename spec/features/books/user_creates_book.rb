9e61967d29f19b9160a6e59a820436f7cae06c35
Jacinda Zhong <jacindaz@gmail.com>
2014-07-04 22:43:22 -0400

    Installs rails helper and spec helper, tests ready to go

--- spec/features/books/user_creates_book.rb
+++ spec/features/books/user_creates_book.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 feature 'user creates a new book' do
 
   scenario 'user submits a new book successfully' do
+    book = FactoryGirl.create(:book)
+    visit new_book_path
+
+    expect(page).to have_content book.title
+    expect(page).to have_content book.author
+    expect(page).to have_content book.year
   end
 
   scenario 'user submits a blank book' do

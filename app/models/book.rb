d0dc1377b17601b6c6680e3d5efabaaaa5924f9d
Jacinda Zhong <jacindaz@gmail.com>
2014-07-06 13:23:53 -0400

    Adds additional validations to models
     - book model: title/author must be unique,
                   author must be between 2-6 words
                   year must be between 1800-2014
     - review model: rating between 1-5
    		 body btwn 2-300 words
    		 book id must be integer

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,13 @@
 class Book < ActiveRecord::Base
   validates :title, presence: true, uniqueness: { scope: :author }
-  validates :author, presence: true
-  validates :year, presence: true
+  validates :author, presence: true, length: {
+    minimum: 2,
+    maximum: 6,
+    tokenizer: lambda { |str| str.scan(/\w+/) },
+    too_short: "Author must have at least %{count} words.",
+    too_long: "Author must have less than %{count} words."
+  }
+  validates :year, presence: true, inclusion: { within: 1800..2014 }
 
   has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,14 @@
 class Review < ActiveRecord::Base
-  validates :rating, presence: true
-  validates :body, presence: true
-  validates :book_id, presence: true
+  validates :rating, presence: true, inclusion: { within: 1..5 }
+  validates :body, presence: true, length: {
+    minimum: 2,
+    maximum: 300,
+    tokenizer: lambda { |str| str.scan(/\w+/) },
+    too_short: "Must have at least %{count} words.",
+    too_long: "Must have less than %{count} words."
+  }
+  validates :book_id, presence: true,
+            numericality: { integer: true }
 
   belongs_to :book
 end

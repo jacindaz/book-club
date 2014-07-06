11df5e2b4f0c8bf59621633a5a8736f623be0d26
Jacinda Zhong <jacindaz@gmail.com>
2014-07-06 09:35:02 -0400

    Successfully adds new reviews for a particular book

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+    @book = Book.find(params[:book_id])
+  end
+
+  def create
+    @book = Book.find(params[:book_id])
+    @review = Review.new(review_params)
+    @review.book = @book
+
+    if @review.save
+      flash[:notice] = "Review saved."
+      redirect_to book_path(@book)
+    else
+      flash.now[:notice] = "Review not saved."
+      render :new
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :body)
+  end
+
 end
--- spec/features/reviews/user_enters_new_review_spec.rb
+++ spec/features/reviews/user_enters_new_review_spec.rb
@@ -3,6 +3,16 @@ require 'rails_helper'
 feature 'user creates a new review' do
 
   scenario 'user submits a new review successfully' do
+    review = FactoryGirl.create(:review)
+    book = FactoryGirl.create(:book)
+    visit new_book_review_path(book)
+
+    select "Rating", with: review.rating
+    fill_in "Your Review", with: review.body
+    click_button "Submit Your Review"
+
+    expect(page).to have_content review.rating
+    expect(page).to have_content review.body
   end
 
   scenario 'user submits a blank review' do

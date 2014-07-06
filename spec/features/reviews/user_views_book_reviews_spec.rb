5d9d264eb557860a8c1f90f01faf3017c32ce792
Jacinda Zhong <jacindaz@gmail.com>
2014-07-06 15:24:43 -0400

    Passes all reviews and books tests

--- spec/features/reviews/user_views_book_reviews_spec.rb
+++ spec/features/reviews/user_views_book_reviews_spec.rb
@@ -3,6 +3,18 @@ require 'rails_helper'
 feature 'user views reviews for a book' do
 
   scenario 'user views all reviews for a single book' do
+    book = FactoryGirl.create(:book)
+    reviews = FactoryGirl.create_list(:review, 3)
+
+    reviews.each do |review|
+      visit new_book_review_path(book)
+      select review.rating, from: "Rating"
+      fill_in "Your Review", with: review.body
+      click_button "Submit Your Review"
+
+      expect(page).to have_content review.rating
+      expect(page).to have_content review.body
+    end
 
   end
 
